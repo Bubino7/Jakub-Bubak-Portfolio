d05570ee6108136d203c263d2d90a6eaaa27bb1e
Jakub Bubak <bubakjakub007@gmail.com>
2024-10-30 08:27:18 +0100

    Finished the page until the footer section. Almost finished with the whole project.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,25 @@
  * @license Apache-2.0
  */
 
+
+/**
+ * Node modules
+ */
+import React from "react";
+
+
+
 /**
  * Components
  */
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
+import Skill from "./components/Skill.jsx";
+import Work from "./components/Work.jsx";
+import Review from "./components/Review.jsx";
+import Contact from "./components/Contact.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
 
@@ -16,7 +30,13 @@ const App = () => {
       <Header />
       <main>
         <Hero />
+        <About />
+        <Skill />
+        <Work />
+        <Review />
+        <Contact />
       </main>
+      <Footer />
     </>
   );
 
